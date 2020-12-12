cf8d6ddee1436fe8cb192cec6cea89cb8717404f
Siddhant Jha <60434913+Siddhant-Jha@users.noreply.github.com>
2020-12-12 19:52:33 +0530

    Added Table
    Added Table

---
+++ table.py
@@ -0,0 +1,4 @@
+print("Welcome To The Multiplication Table App")
+number = float(input("Enter The Number You Want To Work With:\t"))
+for i in range(11):
+    print(str(number) + " * " +str(i)+ " = " + str(number*i))
