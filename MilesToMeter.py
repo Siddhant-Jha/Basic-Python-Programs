041f8003060caf55a5afd5b88cbc133ee3d08162
Siddhant Jha <60434913+Siddhant-Jha@users.noreply.github.com>
2020-12-12 19:51:40 +0530

    Added MilesToMeter
    Added MilesToMeter

---
+++ MilesToMeter.py
@@ -0,0 +1,6 @@
+print("Welcome To The Miles Per Hour To Meter Per Second Conversion app")
+miles = float(input("Enter Your Speed In Miles Per Hour :\t"))
+mps = miles*0.4474
+mps = round(mps,2)
+print("Your Speed In Miles Per Hour Is : " + str(miles))
+print("While, Your Speed In Meter Per Second Is : "+ str(mps))
